5c7763cf0a2388c8239ee5bdc6644ea28f852bad
tian <935365539@qq.com>
2016-08-05 18:41:20 +0800

    no reson

--- test1/src/main/java/com/hand/App.java
+++ test1/src/main/java/com/hand/App.java
@@ -2,6 +2,7 @@ package com.hand;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.List;
 
 import org.apache.commons.io.FileUtils;
 import org.junit.Test;
@@ -27,6 +28,12 @@ public class App
 		try {
 			File file = new File("text.txt");
 			String string = FileUtils.readFileToString(file,"UTF-8");
+			
+			
+			List<String> lines = FileUtils.readLines(file,"utf-8");
+			
+			System.out.println("111111111111111111111");
+			System.out.println(lines.get(0));
 			
 			System.out.println(string);
 			
